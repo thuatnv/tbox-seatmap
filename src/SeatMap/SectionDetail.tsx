35c1f13a9d4ce69b69a9974592d78f7e389d3ef1
thuatnv <thuat.nguyen2@tiki.vn>
2023-08-09 11:23:56 +0700

    chore: demo

--- src/SeatMap/SectionDetail.tsx
+++ src/SeatMap/SectionDetail.tsx
@@ -1,8 +1,113 @@
+import React from 'react';
+import dataSection from '../mock/section.json';
+import Section from './components/Section';
+import { Stage, Layer } from "./react-konva";
+
+
 const SectionDetail = () => {
+  console.log(dataSection);
+  const containerRef = React.useRef(null);
+  const stageRef = React.useRef(null);
+
+  const [scale, setScale] = React.useState(1);
+  const [scaleToFit, setScaleToFit] = React.useState(1);
+  const [size, setSize] = React.useState({
+    width: 1000,
+    height: 1000,
+    virtualWidth: 1000
+  });
+  const [virtualWidth, setVirtualWidth] = React.useState(1000);
+
+  const [selectedSeatsIds, setSelectedSeatsIds] = React.useState([]);
+
+  const [popup, setPopup] = React.useState({ seat: null });
+  // calculate available space for drawing
+  React.useEffect(() => {
+    const newSize = {
+      width: containerRef?.current?.offsetWidth,
+      height: containerRef?.current?.offsetHeight
+    };
+    if (newSize.width !== size.width || newSize.height !== size.height) {
+      setSize(newSize);
+    }
+  });
+
+  // calculate initial scale
+  React.useEffect(() => {
+    if (!stageRef.current) {
+      return;
+    }
+    const stage: any = stageRef.current;
+    const clientRect = stage?.getClientRect({ skipTransform: true });
+
+    const scaleToFit = size.width / clientRect.width;
+    setScale(scaleToFit);
+    setScaleToFit(scaleToFit);
+    setVirtualWidth(clientRect.width);
+  }, [dataSection, size]);
+
+  // togle scale on double clicks or taps
+  const toggleScale = React.useCallback(() => {
+    if (scale === 1) {
+      setScale(scaleToFit);
+    } else {
+      setScale(1);
+    }
+  }, [scale, scaleToFit]);
+
+  let lastSectionPosition = 0;
+
+  const handleHover = React.useCallback((seat: any, pos: any) => {
+    setPopup({
+      seat: seat,
+      position: pos
+    });
+  }, []);
+
+  const handleSelect = React.useCallback(
+    seatId => {
+      const newIds = selectedSeatsIds.concat([seatId]);
+      setSelectedSeatsIds(newIds);
+    },
+    [selectedSeatsIds]
+  );
+
+  const handleDeselect = React.useCallback(
+    seatId => {
+      const ids = selectedSeatsIds.slice();
+      ids.splice(ids.indexOf(seatId), 1);
+      setSelectedSeatsIds(ids);
+    },
+    [selectedSeatsIds]
+  );
+
   return (
-    <>
-      halo 1234
-    </>
+    <div
+    style={{
+      position: "relative",
+      backgroundColor: "lightgrey",
+      width: "100vw",
+      height: "100vh"
+    }}
+    ref={containerRef}
+  >
+    <Stage
+      ref={stageRef}
+      width={size.width}
+        height={size.height}
+      draggable={false}
+      scaleX={scale}
+      scaleY={scale}
+    >
+      <Layer>
+        <Section
+          dataSection={dataSection}
+        />
+      </Layer>  
+    </Stage>
+    </div>
+    
+    
   );
 }
 
---
+++ src/SeatMap/components/Seat.tsx
@@ -0,0 +1,64 @@
+import { Circle } from "../react-konva";
+import { SEAT_SIZE } from "../utils/layout";
+import { SeatProps } from "src/types/seatMap";
+
+function getColor(isBooked: boolean, isSelected: boolean): string {
+  if (isSelected) {
+    return "red";
+  } else if (isBooked) {
+    return "lightgrey";
+  } else {
+    return "#1b728d";
+  }
+}
+
+const Seat = (props: SeatProps)=> {
+  const isBooked = props.data.status === "booked";
+  const { onHover, onDeselect, onSelect, isSelected} = props;
+  return (
+    <Circle
+      x={props.x}
+      y={props.y}
+      radius={SEAT_SIZE / 2}
+      fill={getColor(isBooked, isSelected)}
+      strokeWidth={1}
+      onMouseEnter={e => {
+        e.target._clearCache();
+        onHover(props.data.name, e.target.getAbsolutePosition());
+        const container: any = e.target?.getStage()?.container();
+        if (isBooked) {
+          container.style.cursor = "not-allowed";
+        } else {
+          container.style.cursor = "pointer";
+        }
+      }}
+      onMouseLeave={e => {
+        onHover("", null);
+        const container: any = e.target?.getStage()?.container();
+        container.style.cursor = "";
+      }}
+      onClick={e => {
+        if (isBooked) {
+          return;
+        }
+        if (isSelected) {
+          onDeselect(props.data.name);
+        } else {
+          onSelect(props.data.name);
+        }
+      }}
+      onTap={(e: any) => {
+        if (isBooked) {
+          return;
+        }
+        if (props.isSelected) {
+          props.onDeselect(props.data.name);
+        } else {
+          props.onSelect(props.data.name);
+        }
+      }}
+    />
+  );
+};
+
+export default Seat;
