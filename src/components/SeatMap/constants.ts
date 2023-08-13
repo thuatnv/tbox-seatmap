60e72b0ace75b21777b9bb546e80633b8482d00b
an.le6 <an.le6@tiki.vn>
2023-08-13 21:52:11 +0700

    feat: ❗️❗️ scaling (click and wheel) limits, wheel and btn consistency

--- src/components/SeatMap/constants.ts
+++ src/components/SeatMap/constants.ts
@@ -1,4 +1,7 @@
 export const scaleBy: number = 0.25;
+export const minScale: number = 0.25;
+export const maxScale: number = 10.0;
+export const wheelVsBtnOffset: number = 0.05;
 export const initStageW: number = 900;
 export const initStageH: number = 750;
 export const initMobileStageW: number = 375;
