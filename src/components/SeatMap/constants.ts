bce2624f1f26b1a03988a2f7a7c89af55ebe041d
an.le6 <an.le6@tiki.vn>
2023-08-14 00:01:47 +0700

    chg: adjust scaling values

--- src/components/SeatMap/constants.ts
+++ src/components/SeatMap/constants.ts
@@ -1,6 +1,6 @@
 export const scaleBy: number = 0.25;
 export const minScale: number = 0.25;
-export const maxScale: number = 10.0;
+export const maxScale: number = 5.0;
 export const wheelVsBtnOffset: number = 0.05;
 export const initStageW: number = 900;
 export const initStageH: number = 750;
