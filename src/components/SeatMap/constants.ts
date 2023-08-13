66b05f74d49f5b2b1c7bbf95da7cca42466860c6
an.le6 <an.le6@tiki.vn>
2023-08-13 14:34:23 +0700

    feat: handle zoom in and out, and rest btn disabled state

--- src/components/SeatMap/constants.ts
+++ src/components/SeatMap/constants.ts
@@ -1,4 +1,4 @@
-export const scaleBy: number = 1.05;
+export const scaleBy: number = 0.25;
 export const initStageW: number = 900;
 export const initStageH: number = 750;
 export const initMobileStageW: number = 375;
